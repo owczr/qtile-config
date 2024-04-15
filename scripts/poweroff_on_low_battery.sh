04caf045df6fea143329b3d6b0161cc3e4984465
Jakub Owczarek <jakub.jan.owczarek@gmail.com>
2024-04-15 17:31:31 +0200

    feat: improve the shutdown script

--- scripts/poweroff_on_low_battery.sh
+++ scripts/poweroff_on_low_battery.sh
@@ -1,15 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-while [[ 1 ]]; do
+kill_processes() {
+	# kills process other than itself
+	for pid in $(pgrep -u $USER); do
+		if [[ "$pid" != "$$" ]]; then
+			kill -SIGTERM "$pid"
+		fi
+	done
+}
+
+while true; do
 	battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
 
-	if [[ battery_level -lt 5 ]]; then
+	if [[ "$battery_level" -lt 5 ]]; then
 		notify-send "Battery level critical." "Battery level reached critical level. Shutting down PC." -u critical
 
-		for i in {5..1}; do
+		for ((i = 5; i > 0; i--)); do
 			notify-send "Shutdown" "Shutting down PC in $i." -u critical
 			sleep 1
 		done
+
+		kill_processes
+
+		sleep 5
 
 		poweroff
 	fi
